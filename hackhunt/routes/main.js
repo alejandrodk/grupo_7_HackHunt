29b21313e277b4e81ea5d43d2bd4bf70c2a96473
Federico Garcia <garciaf@dominioba.local>
2020-01-14 17:15:26 -0300

    se agrega multer para la vista de registro de empresas para subir el avatar. Aun no se modificó el objeto que guarda cada empresa en el json. este commit es para guardar los datos que realicé en el trabajo y seguir en casa

--- hackhunt/routes/main.js
+++ hackhunt/routes/main.js
@@ -1,7 +1,7 @@
 // ************ Require's ************
 const express = require("express");
 const router = express.Router();
-
+const upload = require('../middlewares/multer.js');
 // ************ Controller Require ************
 const mainController = require("../controllers/mainController");
 
@@ -16,7 +16,7 @@ router.post("/registro", mainController.valRegUsuario);
 router.get("/empresa/login", mainController.loginEmpresa);
 router.post("/empresa/login", mainController.validarEmpresa);
 router.get("/empresa/registro", mainController.registroEmpresa);
-router.post("/empresa/registro", mainController.valRegEmpresa);
+router.post("/empresa/registro",upload.single('company_avatar'), mainController.valRegEmpresa);
 router.post("/recuperar", mainController.recuperar);
 router.post("/empresa/recuperar", mainController.recuperarEmpresa);
 
