c7146697ec005a4ae63a1d03d0afab8d26bc6729
alejandrodk <ottoabarriosp@hotmail.com>
2020-03-20 13:56:15 -0300

    API clientes

---
+++ hackhunt/api/controllers/clientesController.js
@@ -0,0 +1,12 @@
+const db = require('../../database/models');
+const Sequelize = require("sequelize")
+const Op = Sequelize.Op;
+
+module.exports = {
+    clientes : (req, res) => {
+
+    },
+    cliente : (req, res) => {
+
+    }
+}
---
+++ hackhunt/api/routes/clientes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers/clientesController');
+
+router.get('/', controller.clientes)
+
+module.exports = router;
