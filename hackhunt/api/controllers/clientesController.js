5d4eca9b714f55537b632483147f4c73dd989df4
alejandrodk <ottoabarriosp@hotmail.com>
2020-03-21 13:27:13 -0300

    borrar skill API

--- hackhunt/api/controllers/clientesController.js
+++ hackhunt/api/controllers/clientesController.js
@@ -12,10 +12,10 @@ module.exports = {
     skills : (req, res) => {
         let id = req.params.id;
 
-        db.user_skill.findAll({
+        db.clientes.findOne({
             where : { user_id : id },
-            //include : [{ model: db.skills, as: 'skill', attributes : ['skill_name']}],
-            //raw : true,
+            attributes : ['user_id'],
+            include : [{ model: db.skills, as: 'skill', attributes : ['skill_name'], raw: true }],
         })
         .then(response => {
             if(response){
@@ -39,6 +39,24 @@ module.exports = {
             id : 0,
             user_id : user_id,
             skill_id : skill_id
+        })
+        .then(response => {
+            if(response){
+            return res.json(response);
+            }
+            return res.status(404).json({
+                status : res.statusCode
+            })
+        }).catch(error => console.log(error))
+    },
+    deleteSkill : (req, res) => {
+        let { skill_id } = req.body;
+        
+        db.user_skill.destroy({
+            where : {
+                user_id : req.session.user_id,
+                skill_id : skill_id
+            }
         })
         .then(response => {
             if(response){
